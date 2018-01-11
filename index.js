2e9b0d8e13fa8bb7b2521f31135ec1c5bbb3f133
Yulia Drozdova <yuliannka@gmail.com>
2018-01-11 17:06:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
-  
-  
-  
-  
+  var Band = [];
+  for ( var i=0; i<musicians.length; i++) {
+    Band.push(musicians[i] + "plays" + instruments[i])
+  }
+  return Band
 }
