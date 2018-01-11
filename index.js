773c8f441077119777cfba9dc36bfdd960072e6f
Yulia Drozdova <yuliannka@gmail.com>
2018-01-11 17:49:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function johnLennonFacts(facts) {
 
 function iLoveTheBeatles(n) {
   var array = [];
-  
   do {
-    array.push("I love the Beatles!")
-  } while ( array.length > 0 && maybeTrue());
+    array.push("I love the Beatles!");
+    n++;
+  } while ( n < 15);
   return array
 }
